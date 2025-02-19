153c406fce13a5900479c09200d5095c9a3282bc
Sohum Pulijal <139309457+spulijal13@users.noreply.github.com>
2025-02-19 15:04:34 -0800

    something to try 4

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+cd /app || exit
+
+echo "Applying database migrations..."
 python manage.py migrate
-python data/import_data.py
+
+echo "Collecting static files"
 python manage.py collectstatic --noinput
+
+echo "starting django server"
 exec python manage.py runserver 0.0.0.0:8000
 
 if [ "$DATABASE" = "postgres" ]
@@ -13,6 +20,22 @@ then
     done
 
     echo "PostgreSQL started"
+fi
+
+TABLE_COUNT=$(python /app/manage.py dbshell <<EOF
+SELECT COUNT(*) FROM rna_exonconservation;
+EOF
+)
+
+# Remove whitespace from TABLE_COUNT
+TABLE_COUNT=$(echo "$TABLE_COUNT" | tr -d '[:space:]')
+
+# ✅ If TABLE_COUNT is 0 (empty table), run `import_data.py`
+if [[ "$TABLE_COUNT" -eq "0" ]]; then
+    echo "rna_exonconservation is empty. Running import_data.py..."
+    python /app/data/import_data.py
+else
+    echo "rna_exonconservation already has data. Skipping import_data.py."
 fi
 
 # python manage.py flush --no-input
