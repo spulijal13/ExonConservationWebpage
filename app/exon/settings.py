af093325182f5b333e46eb191e96f068431a10e7
Sohum Pulijal <pulijals@uci.edu>
2025-04-25 16:19:53 -0700

    gunicorn addition

--- app/exon/settings.py
+++ app/exon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","[::1]"]
 
 
 # Application definition
