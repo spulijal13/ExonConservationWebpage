afeddb82a2a7f2089a7f2bb81fff311a8475451e
Sohum Pulijal <pulijals@uci.edu>
2025-04-26 19:59:49 -0700

    something else to try 2

--- app/exon/settings.py
+++ app/exon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","[::1]"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","[::1]", os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 
 
 # Application definition
