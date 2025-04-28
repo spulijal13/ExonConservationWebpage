6eb73321b2cf4d231b7bbc9236ba6f0f187b263d
Sohum Pulijal <pulijals@uci.edu>
2025-04-27 18:45:35 -0700

    changes entrypoint and requirements

--- app/exon/settings.py
+++ app/exon/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [BASE_DIR / "static" ]
+STATICFILES_DIRS = [BASE_DIR / "app" / "static" ]
 
 MEDIA_URL = "media/"
 
