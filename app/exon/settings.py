d1e20aca8b3b01eaa05f41485756577e7d9a6c62
Sohum Pulijal <pulijals@uci.edu>
2025-05-21 13:01:16 -0700

    restart

--- app/exon/settings.py
+++ app/exon/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '6lj!%fy$0-p75^htp0s+h^gf9)p&%#oe-cst=y4&z@xrr8xiv_' #os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = True # bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","[::1]"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","[::1]", "ExonConservation.onrender.com"]
 
 
 # Application definition
@@ -75,15 +76,11 @@ WSGI_APPLICATION = "exon.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.environ.get("SQL_DATABASE", "dev_db"),
-        "USER": os.environ.get("SQL_USER", "sohump"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "sohump"),
-        "HOST": os.environ.get("SQL_HOST", "db"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
-    }
+    'default': dj_database_url.config(
+        default='postgresql://sohump:xELfIkdgHuIyI6zTbtt3HAxXuppXbXEG@dpg-d0hal4juibrs739i7fd0-a.oregon-postgres.render.com/dev_db_4evi'
+    )
 }
 
 
