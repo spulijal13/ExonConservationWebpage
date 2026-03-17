0b1ffabd0e5093ccac97a188f1dde41c08ca722b
Sohum Pulijal <pulijals@uci.edu>
2026-03-16 18:19:46 -0700

    updated database url

--- app/exon/settings.py
+++ app/exon/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "exon.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://sohump:o4DuDolCYqwgla43D2RsDAe5fo0q8xBc@dpg-d5c41dre5dus7385g28g-a.oregon-postgres.render.com/dev_db_nsu1'
+        default='postgresql://sohump:brJ4FUPBGHfqzx5z1Cmk7h0xv1qthXOZ@dpg-d6saiuc50q8c73fgapi0-a.oregon-postgres.render.com/dev_db_eib4'
     )
 }
 
